c3eaf45e47da74a9ac7ec2776ebc383ad1ade4ed
Kare Morstol <kare@nottoobadsoftware.com>
2017-03-16 21:51:23 +0100

    Do not require Xcode.
    Swift exists outside of it too you know.

--- linuxmain-generator.rb
+++ linuxmain-generator.rb
@@ -7,8 +7,6 @@ class LinuxmainGenerator < Formula
   url "https://github.com/kareman/linuxmain-generator/archive/0.1.0.tar.gz"
   sha256 "164256d2e7535545d88ff2b0a70b1574e7e4eae605cd5d29f8911f172e1f49f6"
 
-  depends_on :xcode => ["8.2", :build]
-
   def install
     system "unset CC; swift build -c release"
     bin.install ".build/release/linuxmain-generator"
